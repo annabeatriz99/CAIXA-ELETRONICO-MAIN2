48cd57e100bf2c5d7ad6890f380dc39dce61dbe0
Anna Beatriz <55760039+annabeatriz99@users.noreply.github.com>
2023-05-16 12:02:42 -0300

    new commit

---
+++ routes/Clientes.js
@@ -0,0 +1,99 @@
+const express = require("express")
+const router = express.Router()
+const Clientes = require("../service/Clientes")
+const { Client } = require("pg")
+
+router.get("/clientes", async (req, res) => {
+    try {
+        const clientes = await Clientes.getClientes()
+        // console.log(clientes)
+        res.send(clientes)
+    } catch (e) {
+        console.log(e)
+        res.status(500).send({ error: true, message: e.toString() })
+    }
+})
+
+router.get("/clientes/:id", async (req, res) => {
+    try{
+        const client = await Clientes.getClientById(req.params.id) 
+        if(client.length === 0) return res.status(404).send({ error : true, message: "Id não encontrado!"})
+        res.send(client[0])
+    }catch(e){
+        console.log(e)
+        res.status(500).send({ error: true, message: e.toString() })
+    }
+})
+
+router.post("/clientes", async (req, res) => {
+    try{
+
+        const result = await Clientes.create(req.body)
+        res.send(result)
+
+    }catch(e){
+        console.log(e)
+        res.status(500).send({ error : true, message: e.toString()})
+    }
+})
+
+
+router.delete("/clientes/:id", async (req, res) => {
+    try{
+
+        const id = req.params.id
+        const result = await Clientes.deleteById(id)
+        res.send(result)
+
+    }catch(e){
+        console.log(e)
+        res.status(500).send({ error: true, message: e.toString() })
+    }
+})
+
+router.put("/clientes/:id", async(req, res) =>{
+    try{
+        const id = req.params.id
+        const result = await Clientes.updateByClientes(id, req.body)
+        res.send(result)
+    }
+    catch(e){
+        console.log(e)
+        res.status(500).send({ error: true, message: e.toString() })
+    }
+})
+
+router.post("/clientes/depositoByClientes", async (req, res) => {
+    try{
+        
+        const result = await Clientes.depositoByClientes(req.body)
+        res.send(result)
+
+    }catch(e){
+        console.log(e)
+        res.status(500).send({ error : true, message: e.toString()})
+    }
+})
+
+router.post("/clientes/saqueByCliente", async (req, res) =>{
+    try{
+        const result = await Clientes.saqueByCliente(req.body)
+        res.send(result)
+    }
+    catch(e){
+        console.log(e)
+        res.status(500).send({ error : true, message: e.toString()})
+    }
+})
+router.post("/clientes/transferenciaByClientes", async (req, res) =>{
+    try{
+      const result = await Clientes.transferenciaByClientes(req.body)
+        res.send(result)
+    }catch(e){
+        console.log(e)
+        res.status(500).send({ error : true, message: e.toString()})
+    }
+})
+
+
+module.exports = router
---
+++ service/Clientes.js
@@ -0,0 +1,108 @@
+const { query } = require("express")
+const db = require("../db")
+
+const getClientes = async () => {
+    const con = await db.connect()
+    const res = await con.query("SELECT * FROM clientes")
+    return res.rows
+}
+
+const getClientById = async (id) => {
+    const con = await db.connect()
+    const res = await con.query(`SELECT * FROM clientes WHERE id = ${id}`)
+    return res.rows
+}
+
+const create = async ({ nome, idade, senha, numero_conta, cpf }) => {
+    const con = await db.connect()
+    const res = await con.query(`INSERT INTO clientes (nome, idade, senha, numero_conta, cpf) VALUES ('${nome}', ${idade}, '${senha}', '${numero_conta}', '${cpf}')`)
+    return res.rows
+}
+
+const deleteById = async (id) => {
+    const con = await db.connect()
+    const res = await con.query (`DELETE FROM clientes WHERE id = ${id}`)
+    return res.rows
+}
+
+ const updateByClientes = async (id,{nome, idade, senha, numero_conta, cpf}) =>{
+    const con = await db.connect()
+    const res = await con.query (`update clientes
+    set nome = '${nome}', idade = ${idade}, senha = '${senha}', numero_conta = ${numero_conta}, cpf = ${cpf} where id = ${id}`)
+    return res.rows
+}
+
+const depositoByClientes = async ({saldo_em_real,numero_conta}) =>{
+
+    const con = await db.connect()
+    const clientes = await con.query(`select * from clientes where numero_conta = ${numero_conta}`) //trazer conta
+
+    if( clientes.rows.length == 0){  
+        throw new Exception("conta nao encontrada")
+    } //verificação
+    const cliente = clientes.rows[0]     //pegando a primeira linha do array de clientes
+
+    const novo_saldo = saldo_em_real + cliente.saldo_em_real   //pegando o saldo e somando com o valor anterior
+    
+    const res = await con.query (`update clientes set saldo_em_real = ${novo_saldo} where numero_conta =${numero_conta}`)
+    return res.rows
+} //atualizando o resgitro no banco
+
+
+const saqueByCliente = async({saldo_em_real, numero_conta}) =>{
+    const con = await db.connect()
+    const clientes = await con.query(`select * from clientes where numero_conta = ${numero_conta}`)
+
+    if( clientes.rows.length === 0){  
+        throw new Exception("conta nao encontrada")
+    }
+    const cliente = clientes.rows[0]
+
+    if (saldo_em_real > cliente.saldo_em_real) {
+        throw new Exception("Saldo insuficiente");
+      }
+   
+    const novo_saldo = cliente.saldo_em_real - saldo_em_real
+    
+        const res = await con.query (`update clientes set saldo_em_real = ${novo_saldo}  where numero_conta =${numero_conta}`)
+    return res.rows
+
+}
+const transferenciaByClientes = async ({ saldo_em_real, conta_origem, conta_destino }) => {
+    const con = await db.connect();
+    const clientesOrigem = await con.query(`SELECT * FROM clientes WHERE numero_conta = ${conta_origem}`);
+    const clientesDestino = await con.query(`SELECT * FROM clientes WHERE numero_conta = ${conta_destino}`);
+  
+    if (clientesOrigem.rows.length === 0 || clientesDestino.rows.length === 0) {
+      throw new Exception("Conta não encontrada");
+    }
+     
+    const clienteOrigem = clientesOrigem.rows[0];
+    const clienteDestino = clientesDestino.rows[0];
+  
+    if (saldo_em_real > clienteOrigem.saldo_em_real) {
+      throw new Exception("Saldo insuficiente");
+    }
+  
+    const novo_saldo_origem = clienteOrigem.saldo_em_real - saldo_em_real;
+    const novo_saldo_destino = clienteDestino.saldo_em_real + saldo_em_real;
+  
+    const resOrigem = await con.query(`UPDATE clientes SET saldo_em_real = ${novo_saldo_origem} WHERE numero_conta = ${conta_origem}`);
+    const resDestino = await con.query(`UPDATE clientes SET saldo_em_real = ${novo_saldo_destino} WHERE numero_conta = ${conta_destino}`);
+
+    return res.rows
+}
+
+
+
+
+module.exports = ({
+    getClientes,
+    getClientById,
+    create,
+    deleteById,
+    updateByClientes,
+    depositoByClientes,
+    saqueByCliente,
+    transferenciaByClientes
+})
